541778709ae07281f159b150336c8dee92524f73
MateoLinux <villegasleivam@gmail.com>
2023-04-05 15:03:46 -0300

    4

---
+++ algoritmos/tp2/tp2.c
@@ -0,0 +1,73 @@
+#include "tp2.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct node;
+typedef struct node node_t;
+
+struct node {
+  void *value;
+  node_t *next;
+  node_t *prev;
+};
+
+struct list {
+  node_t *head;
+  node_t *tail;
+  size_t size;
+};
+
+struct list_iter {
+  list_t *list;
+  node_t *curr;
+};
+
+list_t *list_new() {
+  list_t *list = malloc(sizeof(list_t));
+  if (list == NULL)
+    return NULL;
+  list->head = NULL;
+  list->tail = NULL;
+  list->size = 0;
+  return list;
+}
+
+size_t list_length(const list_t *list) { return list->size; }
+
+bool list_is_empty(const list_t *list) { return true; }
+
+bool list_insert_head(list_t *list, void *value) { return false; }
+
+bool list_insert_tail(list_t *list, void *value) { return false; }
+
+void *list_peek_head(const list_t *list) { return NULL; }
+
+void *list_peek_tail(const list_t *list) { return NULL; }
+
+void *list_pop_head(list_t *list) { return NULL; }
+
+void *list_pop_tail(list_t *list) { return NULL; }
+
+void list_destroy(list_t *list, void destroy_value(void *)) { return; }
+
+list_iter_t *list_iter_create_head(list_t *list) { return NULL; }
+
+list_iter_t *list_iter_create_tail(list_t *list) { return NULL; }
+
+bool list_iter_forward(list_iter_t *iter) { return false; }
+
+bool list_iter_backward(list_iter_t *iter) { return false; }
+
+void *list_iter_peek_current(const list_iter_t *iter) { return NULL; }
+
+bool list_iter_at_last(const list_iter_t *iter) { return false; }
+
+bool list_iter_at_first(const list_iter_t *iter) { return false; }
+
+void list_iter_destroy(list_iter_t *iter) { return; }
+
+bool list_iter_insert_after(list_iter_t *iter, void *value) { return false; }
+
+bool list_iter_insert_before(list_iter_t *iter, void *value) { return false; }
+
+void *list_iter_delete(list_iter_t *iter) { return NULL; }
